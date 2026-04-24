ee4110524f5a442334590c2f5b2430c6141e950b
SheesKhan19 <sheesk1000@gmail.com>
2026-04-25 02:04:18 +0500

    Rename project and update author in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="RAG Medcal Chatbot",
+    name="RAG Chatbot",
     version="0.1",
-    author="Sudhanshu",
+    author="SheesKhan",
     packages=find_packages(),
     install_requires = requirements,
 )
