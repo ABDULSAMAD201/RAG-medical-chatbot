89613939e197cf2f98f85ac8da15bee5002ae6ac
Fawad Ajmal <fawad.ajmal12699@gmail.com>
2026-04-25 09:00:57 +0500

    Remove conflict markers from setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="RAG Chatbot",
     version="0.1",
-<<<<<<< HEAD
-    author="SheesKhan",
-=======
-    author="Shees",
->>>>>>> 80f1e9f (Update package name and author in setup.py)
+    author="Shees Khan",
     packages=find_packages(),
     install_requires = requirements,
 )
